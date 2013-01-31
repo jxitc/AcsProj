4363c400cce46d84b90f134a9f84c5f5a2b5a3b6
X.J Home <jxitc@hotmail.com>
2013-01-31 11:24:14 +0000

    Log File added!

--- Util/Log.py
+++ Util/Log.py
@@ -10,22 +10,71 @@ sys.path.append(os.path.join(os.path.dirname(__file__), 'Corpus'))
 
 class Log:
 	"""
-	Log class
+	Log class, use to log files 
 	"""
 	# class static variable
 	RunLogFilePath = "" 
 	RunLogWriter = None
 
+
 	def __init__(self):
+
 		if self.RunLogFilePath == "":
+			# TODO or read log path from config file?
 			self.RunLogFilePath = os.path.join(os.path.dirname(__file__), \
 																				'../Run.log',)
 		else:
 			pass
 
 		if self.RunLogWriter is None:
-			self.RunLogWriter = open(self.RunLogFilePath, 'r')
+			self.RunLogWriter = open(self.RunLogFilePath, 'a') # Start appending
 		
 		print("Start Logging")
-			
+		self.RunLogWriter.write( \
+			"\n\n===============================\n Start logging %s\n %s\n\n" \
+			% (self.GetDateStamp(), __file__) \
+		)
+		
 	
+	def GetDateStamp(self):
+		"""
+		Get current datetime stamp.
+		"""
+		
+		import time
+		timeFmt = "%Y%m%d%H%M%S"
+		timeStr = time.strftime(timeFmt, time.localtime())
+		return timeStr
+
+	def GetTimeStamp(self):
+		"""
+		Get current time stamp.
+		"""
+
+		import time
+		timeFmt = "%H.%M.%S"
+		timeStr = time.strftime(timeFmt, time.localtime())
+		return timeStr
+
+	def WriteLog(self, msg):
+		"""
+		Write message to log file
+		"""
+
+		self.RunLogWriter.write("[%s]\t%s\n" % (self.GetTimeStamp(), msg))
+		
+
+
+
+# Testing...
+if __name__ == '__main__':
+	print('Start module self testing ... ')
+	lg = Log()
+	lg.WriteLog("This is a log message")
+	lg2 = Log()
+	lg2.WriteLog("LG2: I am here!")
+	lg.WriteLog("This is another log message")
+	lg.WriteLog("This is another log message")
+	lg.WriteLog("This is another log message")
+	lg.WriteLog("This is another log message")
+
