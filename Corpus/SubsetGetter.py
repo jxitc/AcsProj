5e6841a4ac766c09e4b4a33a97c214b97a1f8a60
X.J DTAL <jxitc@hotmail.com>
2013-02-13 14:21:57 +0000

    Commited from DTAL Wed Feb 13 14:21:57 GMT 2013

--- Corpus/SubsetGetter.py
+++ Corpus/SubsetGetter.py
@@ -4,6 +4,7 @@ Created on 2013-2-7
 @author: Xiao
 '''
 
+
 import sys,os
 sys.path.append(os.path.join(os.path.dirname(__file__), 'Weka'))
 sys.path.append(os.path.join(os.path.dirname(__file__), 'Util'))
